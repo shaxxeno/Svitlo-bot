83816d5a3d23807062305cbcc4117c8317dc7809
Shaxxeno <novvikovdmitry@gmail.com>
2023-04-07 10:06:16 +0300

    updated

--- tgbot/models/db/users.py
+++ tgbot/models/db/users.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, VARCHAR, DATE, BigInteger, String
+from sqlalchemy import Column, VARCHAR, DATE, BigInteger
 import datetime
 from sqlalchemy.ext.declarative import declarative_base
 
